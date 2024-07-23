843fe7102c4023e67efc56d7e2b8487100a3de02
Dana <Dana.ackerman0914@gmail.com>
2024-07-23 14:06:46 +0500

    Added sql

--- sql/ticket_db.sql
+++ sql/ticket_db.sql
@@ -7,34 +7,7 @@ CREATE TABLE city (
 
 );
 
-CREATE TABLE route (
-    id SERIAL4 NOT NULL,
-    departure_city_id INT4 NOT NULL,
-    arrival_city_id INT4 NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (departure_city_id) REFERENCES city (id),
-    FOREIGN KEY (arrival_city_id) REFERENCES city (id)
-);
 
-
-CREATE TABLE segments (
-    id SERIAL4 NOT NULL,
-    segment INT4 NOT NULL,
-    segment_boundary INT4 NOT NULL,
-    departure_city_id INT4 NOT NULL,
-    arrival_city_id INT4 NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (departure_city_id) REFERENCES city (id),
-    FOREIGN KEY (arrival_city_id) REFERENCES city (id)
-);
-
-CREATE TABLE route_segments (
-    route_id INT4 NOT NULL,
-    segments_id INT4 NOT NULL,
-    PRIMARY KEY(route_id, segments_id),
-    FOREIGN KEY (route_id) REFERENCES route(id),
-    FOREIGN KEY (segments_id) REFERENCES segments(id)
-);
 
 CREATE TABLE traveller (
     id SERIAL4 NOT NULL,
@@ -43,25 +16,28 @@ CREATE TABLE traveller (
     PRIMARY KEY (id)
 );
 
-
+CREATE TABLE route (
+    id SERIAL4 NOT NULL,
+    segment INT4 NOT NULL,
+    departure_city_id INT4 NOT NULL,
+    arrival_city_id INT4 NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (departure_city_id) REFERENCES city (id),
+    FOREIGN KEY (arrival_city_id) REFERENCES city (id)
+);
 
 CREATE TABLE ticket
 (
     id           SERIAL4 NOT NULL,
     traveller_id INT4    NOT NULL,
-    route_id     INT4    NOT NULL,
     price        INT4    NOT NULL,
     currency     CHAR(3) NOT NULL,
-    result       TEXT    NOT NULL CHECK (result in ('success', 'failure')),
-    change       INT4    DEFAULT NULL,
-    lack_of      INT4    DEFAULT NULL,
+    segments INT4 NOT NULL,
+    departure VARCHAR(20) NOT NULL,
+    arrival VARCHAR(20) NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (traveller_id) REFERENCES traveller (id),
-    FOREIGN KEY (route_id) REFERENCES route (id),
-    CHECK (
-        (result = 'success' AND lack_of IS NULL AND change IS NOT NULL) OR
-        (result = 'failure' AND change IS NULL AND lack_of IS NOT NULL)
-        )
+    FOREIGN KEY (traveller_id) REFERENCES traveller (id)
+
 );
 
 INSERT INTO city (name)
@@ -73,30 +49,22 @@ VALUES ('London'),
        ('Coventry'),
        ('Northampton');
 
-INSERT INTO route (departure_city_id, arrival_city_id)
-VALUES (1, 4),
-       (4, 3),
-       (5, 1);
 
-INSERT INTO segments (segment, segment_boundary, departure_city_id, arrival_city_id)
-VALUES (7, 4, 1, 4),
-       (7, 5, 4, 3),
-       (5, 2, 5, 1);
-
-INSERT INTO route_segments (route_id, segments_id)
-VALUES (1, 1),
-       (2, 2),
-       (3, 3);
 
 INSERT INTO traveller (name, traveller_amount)
 VALUES ('Sam', 20),
        ('Kai', 10),
-       ('Katy', 27),
-       ('Nelly', 5);
-INSERT INTO ticket (traveller_id, route_id, price, currency, result, change, lack_of)
-VALUES (3, 1, 25, 'GBP', 'success', 0, NULL),
-       (1, 2, 25, 'GBP', 'failure', NULL, 5),
-       (2, 3, 17, 'GBP', 'failure', NULL, 7);
+       ('Katy', 27);
+
+INSERT INTO ticket (traveller_id, price, currency, segments, departure, arrival)
+VALUES (3,  25, 'GBP', 7, 'London', 'Bristol'),
+       (1,  25, 'GBP', 5,  'Bristol', 'Swinden'),
+       (2,  17, 'GBP', 2,  'Birmingham', 'London');
+
+INSERT INTO route (segment, departure_city_id, arrival_city_id)
+VALUES (7, 1, 4),
+       (5, 4, 3),
+       (2, 5, 1);
 
 
 
