e664b5d69c5780164290ddc55f66b06412b4de94
Dana <Dana.ackerman0914@gmail.com>
2024-07-26 12:36:34 +0500

    Added sql directory

--- sql/ticket_db.sql
+++ sql/ticket_db.sql
@@ -18,9 +18,9 @@ CREATE TABLE traveller (
 
 CREATE TABLE route (
     id SERIAL4 NOT NULL,
-    segment INT4 NOT NULL,
     departure_city_id INT4 NOT NULL,
     arrival_city_id INT4 NOT NULL,
+    segment INT4 NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (departure_city_id) REFERENCES city (id),
     FOREIGN KEY (arrival_city_id) REFERENCES city (id)
@@ -50,7 +50,6 @@ VALUES ('London'),
        ('Northampton');
 
 
-
 INSERT INTO traveller (name, traveller_amount)
 VALUES ('Sam', 20),
        ('Kai', 10),
@@ -61,10 +60,48 @@ VALUES (3,  25, 'GBP', 7, 'London', 'Bristol'),
        (1,  25, 'GBP', 5,  'Bristol', 'Swinden'),
        (2,  17, 'GBP', 2,  'Birmingham', 'London');
 
-INSERT INTO route (segment, departure_city_id, arrival_city_id)
-VALUES (7, 1, 4),
+INSERT INTO route (departure_city_id, arrival_city_id, segment)
+VALUES
+       (5, 6, 1),
        (5, 4, 3),
-       (2, 5, 1);
-
+       (5, 3, 4),
+       (5, 7, 3),
+       (5, 1, 5),
+       (5, 2, 6),
+       (6, 5, 1),
+       (6, 7, 2),
+       (6, 1, 4),
+       (6, 4, 4),
+       (6, 2, 5),
+       (6, 3, 5),
+       (7, 1, 2),
+       (7, 2, 3),
+       (7, 3, 7),
+       (7, 4, 6),
+       (7, 5, 3),
+       (7, 6, 2),
+       (1, 2, 1),
+       (1, 3, 5),
+       (1, 4, 7),
+       (1, 7, 2),
+       (1, 6, 4),
+       (1, 5, 5),
+       (2, 1, 1),
+       (2, 3, 4),
+       (2, 4, 6),
+       (2, 7, 3),
+       (2, 6, 5),
+       (2, 5, 6),
+       (3, 4, 2),
+       (3, 2, 4),
+       (3, 1, 5),
+       (3, 5, 4),
+       (3, 6, 5),
+       (3, 7, 7),
+       (4, 3, 2),
+       (4, 5, 3),
+       (4, 6, 4),
+       (4, 2, 6),
+       (4, 1, 7);
 
 
